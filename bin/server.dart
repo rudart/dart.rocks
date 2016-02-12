ae07b294221520d4b5051eb5c55b4db91d31ab82
Alexey Salnikov <cartrege@gmail.com>
2016-02-12 22:32:14 +0300

    Containerize application

--- bin/server.dart
+++ bin/server.dart
@@ -4,10 +4,9 @@ import "package:redstone/redstone.dart" as app;
 import "package:dart_rocks/config.dart";
 import "package:path/path.dart" show dirname, normalize;
 
-String configPath = normalize(dirname(Platform.script.toFilePath()) + '/..') + '/config.yaml';
-
+String configPath = normalize(dirname(Platform.script.toFilePath()) + '/..') + '/config.yml';
 
 
 main() {
-
+  print("Hello world");
 }
